60f0926074d67ea82a1a077140fff941724c931f
Gammaglobulino <gammaglobulino@ubuntu.ubuntu-domain>
2012-01-13 23:54:43 +0100

    first committ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2372bccbaf9d58b34cb3f9fcbdd937d90ccb2bbf1acc7b19c00bdc16771e5364e947834f13120373ec4ad716d44a5d6a349fed0eba4c75d1ab1cc75d6c4f1f48'
